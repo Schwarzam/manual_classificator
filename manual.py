c35dcb7449a8bcd12367ae2ca1a0c36881901df2
schwarzam <gustavo.b.schwarz@gmail.com>
2020-12-21 20:42:18 -0300

    SSL deactivated

--- manual.py
+++ manual.py
@@ -8,6 +8,19 @@ import pandas as pd
 import requests 
 from io import BytesIO
 
+requests.packages.urllib3.disable_warnings()
+
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 DFROW = ''
 new_df = ''
 dflabel = ''
